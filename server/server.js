14c5f69e7523681fce454ba5a0e48aba2e127286
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-08-09 20:48:42 +0200

    Fixes in the code for remote deployment

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
-var express               = require('express');
-var bodyParser            = require('body-parser');
-var cookieParser          = require('cookie-parser');
-var sReq                  = require('request');
-var qString               = require('querystring');
-var low                   = require('lowdb')
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var sReq = require('request');
+var qString = require('querystring');
+var low = require('lowdb')
 const fileAsync = require('lowdb/lib/storages/file-async')
 
+//Dev
 var githubCredentials = {
-  clientId: 'a096d3ce655c4f0a806f',
-  clientSecret: 'c2d6d65d16c09fa078b2854d5ff8ec7d88d3ab40'
-}
+        clientId: 'a096d3ce655c4f0a806f',
+        clientSecret: 'c2d6d65d16c09fa078b2854d5ff8ec7d88d3ab40'
+    }
+    //Production
+    // var githubCredentials = {
+    //     clientId: '61978f9947c57a58d010',
+    //     clientSecret: '0205e918dee14ea4325aa35b6aabfd314428ca12'
+    // }
 
 //Initialise express (the web server)
 var app = express();
 app.use(cookieParser()); //Cookie parser to set login tokens etc
-app.use(bodyParser.urlencoded({extended: true})); // Return in json format
+app.use(bodyParser.urlencoded({ extended: true })); // Return in json format
 app.use(bodyParser.json()); // same as above.
 var port = process.env.PORT || 8080; //Set port of server (needed in angular app)
 
@@ -22,88 +28,93 @@ var router = express.Router(); //Routing for api
 
 //initialising db (chose lowdb as it's a small solution that we can use for just storing json files)
 var db = low('./server_data.json', {
-  storage: fileAsync
+    storage: fileAsync
 });
 //initialising db defaults
 db.defaults({
-  pageRequests: [],
-  githubRequests: [],
-  userAuths: []
+    pageRequests: [],
+    githubRequests: [],
+    userAuths: []
 }).write();
 
 // on the root path, we're returning a message stating what this api is.
 router.get('/', function(request, response) {
-  response.json({message: "Github SPA for fetching user data"});
+    response.json({ message: "Github SPA for fetching user data" });
 });
 
 //github authentication
 router.get('/authenticate/:hash', function(request, response) {
-  let hash = request.params.hash;
-  console.log("Authenticating user login hash: " + hash);
+    let hash = request.params.hash;
+    console.log("Authenticating user login hash: " + hash);
 
-  sReq.post({url: "https://github.com/login/oauth/access_token", form:{
-    client_id: githubCredentials.clientId,
-    client_secret: githubCredentials.clientSecret,
-    code: hash
-  }}, function(err, resp, body) {
-    //console.log(qString.parse(body))
-    db.get('userAuths').push({access_token: qString.parse(body).access_token}).write();
+    sReq.post({
+        url: "https://github.com/login/oauth/access_token",
+        form: {
+            client_id: githubCredentials.clientId,
+            client_secret: githubCredentials.clientSecret,
+            code: hash
+        }
+    }, function(err, resp, body) {
+        //console.log(qString.parse(body))
+        db.get('userAuths').push({ access_token: qString.parse(body).access_token }).write();
 
-    response.json({access_token: qString.parse(body).access_token});
-  });
+        response.json({ access_token: qString.parse(body).access_token });
+    });
 });
 
 router.get('/server/verify', function(request, response) {
-  let httpStatus = 0;
-  let githubStatus = 0;
-  sReq.get('http://localhost:4200', function(err, resp, body){
-    httpStatus = resp.statusCode;
-    sReq.get('https://api.github.com/', function(err, resp, body){
-      githubStatus = resp.statusCode;
-      if(githubStatus != 0 || githubStatus != 404)
-        githubStatus = "GOOD";
+    let httpStatus = 0;
+    let githubStatus = 0;
+    // Prod : https://githubapp-ab10e.firebaseapp.com
+    // Dev : http://localhost:4200
+    sReq.get('http://localhost:4200', function(err, resp, body) {
+        httpStatus = resp.statusCode;
+        sReq.get('https://api.github.com/', function(err, resp, body) {
+            githubStatus = resp.statusCode;
+            if (githubStatus != 0 || githubStatus != 404)
+                githubStatus = "GOOD";
 
-      response.json({siteRespone: httpStatus, apiStatus: 'GOOD', githubStatus: githubStatus});
+            response.json({ siteRespone: httpStatus, apiStatus: 'GOOD', githubStatus: githubStatus });
+        });
     });
-  });
 });
 
 router.get('/server/saverequest/:type/:user/:endpoint', function(request, response) {
-  let type = request.params.type;
-  let endpoint = request.params.endpoint;
-  let user = request.params.user;
-  let timestamp = new Date();
-  //console.log("Saving request: ", {type: type, endpoint: endpoint, user: user});
-  if(type == "page") {
-    db.get('pageRequests').push({endpoint: endpoint, user: user, time: timestamp.toUTCString()}).write();
-  } else if(type == "github") {
-    db.get('githubRequests').push({endpoint: endpoint, user: user, time: timestamp.toUTCString()}).write();
-  }
-  response.json({type: type, endpoint: endpoint, user: user, timestamp: timestamp.toUTCString()})
+    let type = request.params.type;
+    let endpoint = request.params.endpoint;
+    let user = request.params.user;
+    let timestamp = new Date();
+    //console.log("Saving request: ", {type: type, endpoint: endpoint, user: user});
+    if (type == "page") {
+        db.get('pageRequests').push({ endpoint: endpoint, user: user, time: timestamp.toUTCString() }).write();
+    } else if (type == "github") {
+        db.get('githubRequests').push({ endpoint: endpoint, user: user, time: timestamp.toUTCString() }).write();
+    }
+    response.json({ type: type, endpoint: endpoint, user: user, timestamp: timestamp.toUTCString() })
 });
 
 router.get('/server/accesslog', function(request, response) {
-  let pageRequests    = db.get('pageRequests');
-  let githubRequests  = db.get('githubRequests');
-  let authRequests    = db.get('userAuths');
-  response.json({pageRequests: pageRequests, githubRequests: githubRequests, authRequests: authRequests});
+    let pageRequests = db.get('pageRequests');
+    let githubRequests = db.get('githubRequests');
+    let authRequests = db.get('userAuths');
+    response.json({ pageRequests: pageRequests, githubRequests: githubRequests, authRequests: authRequests });
 });
 
 router.get('/admin/auth/:username', function(request, response) {
-  let username = request.params.username;
-  if(username == "StormBurpee")
-    response.json({auth: true});
-  else {
-    response.json({auth: false});
-  }
+    let username = request.params.username;
+    if (username == "StormBurpee")
+        response.json({ auth: true });
+    else {
+        response.json({ auth: false });
+    }
 });
 
 //Cors
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 // telling the express app to use the router, on the api path (all requests get sent to /api/{request})
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -7,16 +7,25 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LoginService {
-
-  public clientId = "a096d3ce655c4f0a806f";
+  // Dev
+  public clientId = 'a096d3ce655c4f0a806f';
+  // Prod
+  // public clientId = '61978f9947c57a58d010';
   public auth = localStorage.access_token;
 
   urls = {
-    callback: "https://github.com/login/oauth/access_token",
-    callbackServer: "http://localhost:8080/api/authenticate/",
-    api: "https://api.github.com/",
-    localServer: "http://localhost:8080/api/"
-  }
+    callback: 'https://github.com/login/oauth/access_token',
+    // Dev
+    callbackServer: 'http://localhost:8080/api/authenticate/',
+    // callbackServer: 'https://githubapp-ab10e.firebaseapp.com/api/authenticate/',
+    // Prod
+    // Prod
+    api: 'https://api.github.com/',
+    // Dev
+     localServer: 'http://localhost:8080/api/'
+    // Prod
+    // localServer: ' https://githubapp-ab10e.firebaseapp.com/api/'
+  };
 
   constructor( private http: Http ) { }
 
@@ -28,62 +37,62 @@ export class LoginService {
 
   getMyUser(access_token): Observable<any> {
     this.updateServer('github', 'getmyuser:'+access_token);
-    return this.http.get(this.urls.api+"user?access_token="+access_token).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'user?access_token='+access_token).map(this.extractData).catch(this.handleError);
   }
 
   getUser(username): Observable<any> {
     this.updateServer('github', 'getuser:'+username);
-    return this.http.get(this.urls.api+"users/"+username+"?access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'users/'+username+'?access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   getUserRepos(username): Observable<any> {
     this.updateServer('github', 'user_repos:'+username);
-    return this.http.get(this.urls.api+"users/"+username+"/repos"+"?access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'users/'+username+'/repos'+'?access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   getUserEvents(username): Observable<any> {
     this.updateServer('github', 'user_events:'+username);
-    let pg1 = this.http.get(this.urls.api+"users/"+username+"/events?page=1"+"&access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    let pg1 = this.http.get(this.urls.api+'users/'+username+'/events?page=1'+'&access_token='+this.auth).map(this.extractData).catch(this.handleError);
     return pg1;
   }
 
   //DEPRECIATION:
   getUserCommits(username, page): Observable<any> {
     this.updateServer('github', 'user_commits:'+username);
-    return this.http.get(this.urls.api+"users/"+username+"/events?page="+page+"&access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'users/'+username+'/events?page='+page+'&access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   getUserReceived(username, page): Observable<any> {
     this.updateServer('github', 'received_events:'+username);
-    return this.http.get(this.urls.api+"users/"+username+"/received_events?page="+page+"&access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'users/'+username+'/received_events?page='+page+'&access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   getUserStarred(username): Observable<any> {
     this.updateServer('github', 'getstars:'+username);
-    return this.http.get(this.urls.api+"users/"+username+"/starred"+"?access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'users/'+username+'/starred'+'?access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   searchUser(username): Observable<any> {
     this.updateServer('github', 'search:'+username);
-    return this.http.get(this.urls.api+"search/users?q="+username+"&access_token="+this.auth).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.api+'search/users?q='+username+'&access_token='+this.auth).map(this.extractData).catch(this.handleError);
   }
 
   updateServer(type, endpoint) {
     let username = localStorage.username;
     //console.log(type, endpoint, username);
-    this.http.get(this.urls.localServer+"server/saverequest/"+type+"/"+username+"/"+endpoint).subscribe();
+    this.http.get(this.urls.localServer+'server/saverequest/'+type+'/'+username+'/'+endpoint).subscribe();
   }
 
   adminLogin(username): Observable<any> {
-    return this.http.get(this.urls.localServer+"admin/auth/"+username).map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.localServer+'admin/auth/'+username).map(this.extractData).catch(this.handleError);
   }
 
   adminLogs(): Observable<any> {
-    return this.http.get(this.urls.localServer+"server/accesslog").map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.localServer+'server/accesslog').map(this.extractData).catch(this.handleError);
   }
 
   serverDebug(): Observable<any> {
-    return this.http.get(this.urls.localServer+"server/verify").map(this.extractData).catch(this.handleError);
+    return this.http.get(this.urls.localServer+'server/verify').map(this.extractData).catch(this.handleError);
   }
 
   private extractData(res: Response) {
